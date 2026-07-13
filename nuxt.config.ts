28f80dfc62ef6588494b9223638ea650aa10e74a
nopaldonh <nopaldonh21@gmail.com>
2026-07-13 20:38:12 +0700

    chore: install and configure @vueuse/nuxt module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/test-utils/module', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxt/test-utils/module', '@nuxtjs/tailwindcss', '@vueuse/nuxt'],
 })
